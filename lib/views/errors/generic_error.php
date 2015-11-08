b45b7d1981a71b1c50e9d80c8ca7e2688cf8fc48
fterenzani <f.terenzani@gmail.com>
2015-11-08 23:25:45 +0100

    Fixed display errors in FF

--- lib/views/errors/generic_error.php
+++ lib/views/errors/generic_error.php
@@ -1,21 +1,17 @@
-<!doctype>
+<!doctype html>
 <html>
-<head><title><?php echo $statusCode ?> Error</title></head>
+<head><title><?php echo $statusCode ?> Error</title>
 <style>
-body {font: 1em "Trebuchet MS", Helvetica, sans-serif;}
-.container{max-width: 300px; padding: 20px;margin: auto;
-position: relative;
-top: 50%;
--webkit-transform: translateY(-50%);
--ms-transform: translateY(-50%);
-transform: translateY(-50%);
-}
+html,body,table{height: 100%}
+body{font: 1em "Trebuchet MS", Helvetica, sans-serif}
+table{max-width: 300px; margin: auto;vertical-align: center}
 </style>
+</head>
 <body>
-<div class="container">
+<table><tr><td>
 <h1><?php echo $statusCode ?> Error</h1>
 <p><strong>This is an error</strong> and probably it wasn't your fault.
 We'll try to understand if there is something to fix. In the meanwhile
 you can try to go back to the <a href="<?php echo url_for_home() ?>">homepage</a>.
-</div>
+</td></tr></table>
 </body>
